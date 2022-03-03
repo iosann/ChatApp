5c21e921105a6e125e399d09902d4f293b51f5c2
Anna Belousova <annabelousova@MacBook-Air-Anna.local>
2022-03-03 22:09:42 +0400

    Added userIcon and actionSheet

--- ChatApp/BaseProfileView.swift
+++ ChatApp/BaseProfileView.swift
@@ -10,10 +10,24 @@ import UIKit
 class BaseProfileView: UIView {
 
     @IBOutlet private var contentView: UIView!
-    @IBOutlet private weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet private weak var nameTextView: UITextView!
     @IBOutlet private weak var descriptionTextView: UITextView!
     @IBOutlet private weak var saveButton: UIButton!
+    
+    let editIconView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 0.247, green: 0.471, blue: 0.94, alpha: 1)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let editIconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "camera.fill")?.withTintColor(.white, renderingMode: .alwaysOriginal)
+        return imageView
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -35,13 +49,27 @@ class BaseProfileView: UIView {
     }
     
     private func configureView() {
-        photoImageView.backgroundColor = .red
-        photoImageView.layer.cornerRadius = photoImageView.bounds.size.width / 2
-        
         nameTextView.font = UIFont(name: "SFProDisplay-Bold", size: 24)
         descriptionTextView.font = UIFont(name: "SFProText-Regular", size: 16)
         
         saveButton.layer.cornerRadius = 14
-        saveButton.titleLabel?.font = UIFont(name: "SFProText-Regular", size: 19)
+        saveButton.titleLabel?.font = UIFont(name: "SFProText-Semibold", size: 19)
+        
+        contentView.addSubview(editIconView)
+        editIconView.addSubview(editIconImageView)
+        NSLayoutConstraint.activate([
+            editIconView.widthAnchor.constraint(equalToConstant: photoImageView.bounds.size.width / 4),
+            editIconView.heightAnchor.constraint(equalTo: editIconView.widthAnchor),
+            editIconView.bottomAnchor.constraint(equalTo: photoImageView.bottomAnchor),
+            editIconView.trailingAnchor.constraint(equalTo: photoImageView.trailingAnchor),
+            editIconImageView.centerXAnchor.constraint(equalTo: editIconView.centerXAnchor),
+            editIconImageView.centerYAnchor.constraint(equalTo: editIconView.centerYAnchor)
+        ])
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        photoImageView.layer.cornerRadius = photoImageView.bounds.size.width / 2
+        editIconView.layer.cornerRadius = editIconView.bounds.size.width / 2
     }
 }
--- ChatApp/ProfileViewController.swift
+++ ChatApp/ProfileViewController.swift
@@ -25,9 +25,47 @@ class ProfileViewController: UIViewController {
         navigationItem.rightBarButtonItem = closeButton
 
         view = profileView
+        let tap = UITapGestureRecognizer(target: self, action: #selector(editProfileImage))
+        profileView.editIconView.addGestureRecognizer(tap)
     }
     
     @objc private func closeTheScreen() {
         dismiss(animated: true, completion: nil)
+    }
+
+    @objc private func editProfileImage(_ sender: UITapGestureRecognizer) {
+        print("Выбери изображение профиля")
+        
+        let alert = UIAlertController(title: "Choose image source", message: nil, preferredStyle: .actionSheet)
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in
+                imagePicker.sourceType = .camera
+                self.present(imagePicker, animated: true)
+            }
+            alert.addAction(cameraAction)
+        }
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "PhotoLibrary", style: .default) { _ in
+                imagePicker.sourceType = .photoLibrary
+                self.present(imagePicker, animated: true)
+            }
+            alert.addAction(photoLibraryAction)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+}
+
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let selectedImage = info[.originalImage] as? UIImage
+        profileView.photoImageView.image = selectedImage
+        dismiss(animated: true)
     }
 }
